c4ce28f74a350a365c5a18ecd94b2653a37ddf46
vishal acharya <44444485+vishal2457@users.noreply.github.com>
2025-11-30 21:40:15 +0530

    feat: Add TemplateInput struct to define template attributes.

--- derived-parser.go
+++ derived-parser.go
@@ -5,6 +5,16 @@ import (
 	"github.com/deriveddotdev/derived-go-template-parser/internal/resolvers"
 )
 
+type TemplateInput struct {
+	ID           int    `json:"id"`
+	Template     string `json:"template"`
+	Path         string `json:"path"`
+	TemplateType string `json:"templateType"`
+	Pattern      string `json:"pattern"`
+	FileContent  string `json:"fileContent"`
+	Enabled      string `json:"enabled"`
+}
+
 func ResolveMultipleTemplates(templatesInput []models.TemplateInput, data interface{}) models.ResolveTemplatesResponse {
 	return resolvers.ResolveMultipleTemplates(templatesInput, data)
 }
